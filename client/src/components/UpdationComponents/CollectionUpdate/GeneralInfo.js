745839df65fc8a4a02f16683c579f70337d8327f
neo <developersmatrix3x3@gmail.com>
2021-11-29 21:27:22 +0530

    update general info

--- client/src/components/UpdationComponents/CollectionUpdate/GeneralInfo.js
+++ client/src/components/UpdationComponents/CollectionUpdate/GeneralInfo.js
@@ -5,22 +5,30 @@ import styles from "./GeneralInfo.module.css";
 const GeneralInfo = () => {
   return (
     <div className={styles.container__general}>
-      <form className={styles.form__container} onSubmit={null}>
-        <div className={styles.input__container}>
-          <label>Total No of Properties</label>
-          <input type="text" />
+      <div className={styles.heading__primary}>
+        <h1>Property Tax Collection</h1>
+      </div>
+      <form>
+        <div className={styles["form__general--info"]}>
+          <div className={styles.input__container}>
+            <label for="totalnoofproperties">Total No of properties</label>
+            <input type="text" id="totalnoofproperties" />
+          </div>
+          <div className={styles.input__container}>
+            <label for="openingbalance">Opening Balance</label>
+            <input type="text" id="openingbalance" />
+          </div>
+          <div className={styles.input__container}>
+            <label for="currentyeardemand">Current year Demand</label>
+            <input type="text" id="currentyeardemand" />
+          </div>
+          <div className={styles.input__container}>
+            <label for="totaldemand">Total Demand</label>
+            <input type="text" id="totaldemand" disabled />
+          </div>
         </div>
-        <div className={styles.input__container}>
-          <label>Opening Balance</label>
-          <input type="text" />
-        </div>
-        <div className={styles.input__container}>
-          <label>Current year demand</label>
-          <input type="text" />
-        </div>
-        <div className={styles.input__container}>
-          <label>Total demand</label>
-          <input type="text" disabled />
+        <div className={styles.submit__button}>
+          <button type="submit">Update</button>
         </div>
       </form>
     </div>
